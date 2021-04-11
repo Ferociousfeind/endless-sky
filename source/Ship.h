fb55268fddb919c0b15d457c0940dcdac1ca647d
Ferociousfeind <35383563+Ferociousfeind@users.noreply.github.com>
2021-04-11 16:01:40 -0400

    Update Ship.h

--- source/Ship.h
+++ source/Ship.h
@@ -531,6 +531,7 @@ private:
 	int crew = 0;
 	int pilotError = 0;
 	int pilotOkay = 0;
+	double heatDamage = 0;
 	
 	// Current status of this particular ship:
 	const System *currentSystem = nullptr;
